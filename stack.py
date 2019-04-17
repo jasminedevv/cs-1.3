f868400c56d72cfeb7d26f609cc37814124bdec3
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-04-17 14:51:46 -0700

    [add] stack tests passing

--- stack.py
+++ stack.py
@@ -5,13 +5,14 @@ from linkedlist import LinkedList
 
 # Implement LinkedStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
-class LinkedStack(object):
+class LinkedStack(LinkedList):
 
     def __init__(self, iterable=None):
         """Initialize this stack and push the given items, if any."""
         # Initialize a new linked list to store the items
-        self.list = LinkedList()
-        if iterable is not None:
+        super().__init__()
+        self.top = self.head # rename 
+        if iterable is not None: 
             for item in iterable:
                 self.push(item)
 
@@ -22,27 +23,44 @@ class LinkedStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        if self.head == None:
+            return True
+        else:
+            return False
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+        return self.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Push given item
+        self.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
+        try:
+            return self.head.data
+        except AttributeError:
+            return None
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
-
+        try:
+            popped = self.head
+            self.head = self.head.next
+            self.size -= 1
+            return popped.data
+        except AttributeError:
+            raise ValueError("Stack is empty")
+    
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests

0b1742893dda343d7b6d05cfe8f424aa73b0843a
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-05-08 15:23:33 -0700

    [add] set class: needs tests

---
+++ set.py
@@ -0,0 +1,48 @@
+from hashtable import HashTable
+
+class Set(HashTable):
+    def __init__(self, data=None):
+        
+        if data == None:
+            super().__init__()
+
+        elif type(data) != list:
+            super().__init__()
+            self.add(data)
+        else:
+            size = len(data)
+            super().__init__(size*2)
+            for item in data:
+                self.add(item)
+
+    def add(self, item):
+        self.set(item, 0) # we only need the key so set value to 0
+
+    def remove(self, item):
+        self.delete(item)
+
+    def union(self, other_set):
+        new_set = Set(self.keys() + other_set.keys())
+        return new_set
+
+    def difference(self, other_set):
+        difference = Set()
+        for item in self.keys():
+            if not other_set.contains(item):
+                difference.add(item)
+        return difference
+
+    def intersection(self, other_set):
+        intersection = Set()
+        for item in self.keys():
+            if other_set.contains(item):
+                intersection.add(item)
+        return intersection
+
+    def is_subset(self, other_set):
+        """Check if set is a subset of another set"""
+        for item in self.keys():
+            if not other_set.contains(item):
+                return False
+        return True
+
---
+++ set_test.py
@@ -0,0 +1,34 @@
+#!python
+
+from set import Set
+import unittest
+
+class SetTest(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_contains(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_remove(self):
+        pass
+
+    def test_union(self):
+        pass
+
+    def test_intersection(self):
+        pass
+
+    def test_difference(self):
+        pass
+
+    def test_is_subset(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

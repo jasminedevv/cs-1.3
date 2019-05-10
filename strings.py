4fbe7252990f9e655911e3879d4f747196c57c7b
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-05-09 21:06:42 -0700

    [add] find index + buggy find all

---
+++ strings.py
@@ -0,0 +1,94 @@
+#!python
+
+def contains(text, pattern):
+    """Return a boolean indicating whether pattern occurs in text."""
+    assert isinstance(text, str), 'text is not a string: {}'.format(text)
+    assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
+    # TODO: Implement contains here (iteratively and/or recursively)
+
+    return find_index(text, pattern) is not None
+
+
+def find_index(text, pattern):
+    """Return the starting index of the first occurrence of pattern in text,
+    or None if not found."""
+    assert isinstance(text, str), 'text is not a string: {}'.format(text)
+    assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
+    # TODO: Implement find_index here (iteratively and/or recursively)
+    if pattern == "":
+        return 0
+
+    next_char_index = 0
+    for text_index, char in enumerate(text):
+        next_char = pattern[next_char_index] 
+        print("loop stuff:", text_index, char)
+        
+        if char is not next_char:  
+            next_char_index = 0
+            next_char = pattern[next_char_index]
+
+        if char is next_char:
+            print("next char:", next_char)
+            next_char_index += 1
+            if next_char_index >= len(pattern):
+                print("next char index", next_char_index)
+                return text_index - (len(pattern) - 1)
+    
+    return None
+
+
+def find_all_indexes(text, pattern):
+    """Return a list of starting indexes of all occurrences of pattern in text,
+    or an empty list if not found."""
+    assert isinstance(text, str), 'text is not a string: {}'.format(text)
+    assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
+    # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    return list(generate_all_indexes(text, pattern))
+
+
+def generate_all_indexes(text, pattern):
+    patterns = []
+    last_index = 0
+    while last_index <= (len(text) + len(pattern)):
+        print("loop")
+        last_index = find_index(text[last_index:], pattern)
+        print(last_index)
+
+        if last_index is None:
+            break
+
+        yield last_index
+        last_index += 1
+
+
+def test_string_algorithms(text, pattern):
+    found = contains(text, pattern)
+    print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
+    # TODO: Uncomment these lines after you implement find_index
+    index = find_index(text, pattern)
+    print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
+    # TODO: Uncomment these lines after you implement find_all_indexes
+    indexes = find_all_indexes(text, pattern)
+    print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
+
+
+def main():
+    """Read command-line arguments and test string searching algorithms."""
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 2:
+        text = args[0]
+        pattern = args[1]
+        test_string_algorithms(text, pattern)
+    else:
+        script = sys.argv[0]
+        print('Usage: {} text pattern'.format(script))
+        print('Searches for occurrences of pattern in text')
+        print("\nExample: {} 'abra cadabra' 'abra'".format(script))
+        print("contains('abra cadabra', 'abra') => True")
+        print("find_index('abra cadabra', 'abra') => 0")
+        print("find_all_indexes('abra cadabra', 'abra') => [0, 8]")
+
+
+if __name__ == '__main__':
+    main()
